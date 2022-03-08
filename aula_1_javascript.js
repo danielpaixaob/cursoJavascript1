df6fd26d8781e29bcda77b9773cc00b8149dca4a
danielpaixaob <101207653+danielpaixaob@users.noreply.github.com>
2022-03-08 17:31:46 -0300

    exemplos da aula 1

---
+++ aula_1_javascript.js
@@ -0,0 +1,12 @@
+// tipo number (tipo numero)
+
+// 'const' é usado para declarar uma variável
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
---
+++ aula_2_string.js
@@ -0,0 +1,17 @@
+// string são textos
+
+const texto1 = 'Olá, mundo! ';
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456";
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Daniel";
+
+
+// concatenação (+)
+
+console.log(texto1 + citacao + meuNome)
+
+//template string ou template literal
+
